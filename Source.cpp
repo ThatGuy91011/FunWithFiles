d5419a61730ab655c185ced29a52a77cfe16719f
KIRK WALLS <KIRWALLS@uat.edu>
2020-01-30 16:20:14 -0700

    Need to input counted words before words

--- Source.cpp
+++ Source.cpp
@@ -2,18 +2,19 @@
 #include <fstream>
 #include <string>
 
+int keywords, reserved;
+
 int main()
 {
+	/**
 	std::cout << "\t***Memory Machine***" << std::endl;
 	std::cout << "Enter what you need remembered: ";
 	std::string stuffToRemember;
 	std::getline(std::cin, stuffToRemember);
 	//Create object file handle
-	std::ifstream input_file;
 	std::ofstream obj_file_handle;
 	
 	//Opening file 
-	input_file.open("CPP_Reference.txt");
 	obj_file_handle.open("memory.txt");
 	//Using file stream
 	std::cout << "Uploading '" << stuffToRemember << "' to file..." <<std::endl;
@@ -24,4 +25,47 @@ int main()
 	std::cout << "Closing file..." << std::endl;
 	obj_file_handle.close();
 	std::cout << "File closed" << std::endl;
+	**/
+	//Read files
+	std::fstream file;
+	std::string word, filename;
+	std::ofstream obj_file_handle;
+	obj_file_handle.open("CPP_Enhanced_Reference.txt");
+	
+	filename = "CPP_Reference.txt";
+	file.open(filename.c_str());
+
+	while (file >> word)
+	{
+		if (word == "-")
+		{
+			keywords += 1;
+		}
+
+		if (word == ">")
+		{
+			word = "\n";
+		}
+
+		if (word == "Reserved")
+		{
+			break;
+		}
+		obj_file_handle << word << " ";
+	}
+
+	while (file >> word)
+	{
+		if (word == "-")
+		{
+			reserved += 1;
+		}
+
+		if (word == ">")
+		{
+			word = "\n";
+		}
+		obj_file_handle << word << " ";
+	}
+	std::cout << "\n\nKeywords: " << keywords << std::endl << "Reserved Words: " << reserved << std::endl;
 }
